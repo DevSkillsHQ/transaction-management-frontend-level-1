7749f2369f66ab273b02cf25d69ae39e2f8fd760
Anton Fenske <anton@devskills.co>
2023-09-25 22:59:39 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  env: {
-    apiUrl: 'http://host.docker.internal:8080',
-  },
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {},
     supportFile: false,
   },
